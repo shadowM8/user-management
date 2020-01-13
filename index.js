9bdfc186cade7507c6ac6ac58e64eb292d9bcf9a
Antonius Wibisono <antonius.wibisono@myboost.id>
2020-01-13 11:34:36 +0700

    edit code in server to comply eslint

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('./server');
 const config = require('./config');
-const port = config.get('PORT')
+
+const port = config.get('PORT');
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`user management API is listening on port ${port}`);
-})
+});
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express'); // set up an express app
 // for checking validity of required parameter use express-validator
 const expressValidator = require('express-validator');
-const enrouten = require('express-enrouten'); // for easy route configuration 
+const enrouten = require('express-enrouten'); // for easy route configuration
 
 // import Error classes
 // NOTE: UnauthorizedError is built into express-jwt
-const BadRequestError    = require('./errors/bad-request');
+// const BadRequestError = require('./errors/bad-request');
 // let ForbiddenError     = require('./errors/forbidden');
 const RouteNotFoundError = require('./errors/route-not-found.js');
 
 // load environment variables
 // require('dotenv').config();
-const config = require('./config');
+// const config = require('./config');
 
 // connect to MongoDB
 // mng.Promise = global.Promise
@@ -24,21 +24,21 @@ const app = express();
  * Preflight Middleware
  */
 // CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  //intercepts OPTIONS method
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  // intercepts OPTIONS method
+  // eslint-disable-next-line yoda
   if ('OPTIONS' === req.method) {
     res.send(200);
-  }
-  else {
+  } else {
     next();
   }
 });
 
-//validator
-app.use(expressValidator())
+// validator
+app.use(expressValidator());
 
 // parse JSON in the body of requests
 app.use(express.urlencoded({ extended: false }));
@@ -58,8 +58,8 @@ app.use((req, res, next) => {
 });
 
 // handle errors (404 is not technically an error)
-app.use((err, req, res, next) => {
-  switch(err.name) {
+app.use((err, req, res) => {
+  switch (err.name) {
     case 'BadRequestError':
       res.status(400).json({ name: err.name, message: err.message });
       break;
