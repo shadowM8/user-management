109f1de1e7aed81b756df7dfd817b48dacb855fe
Antonius Wibisono <antonius.wibisono@myboost.id>
2020-01-13 10:04:43 +0700

    update eslint and tidying code

--- actions/user.js
+++ actions/user.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-useless-catch */
 const { username: usernameModel } = require('../db/models/postgresql');
-const { comparePass, encryptPass } = require('../libs/password');
-const { jwtSign, jwtVerify } = require('../libs/jwt');
+const { comparePass } = require('../libs/password');
+const { jwtSign } = require('../libs/jwt');
 const config = require('../config');
+
 const key = config.get('KEY_MAKER');
 const shortcut = config.get('SHORTCUT');
 
@@ -9,61 +11,62 @@ const createUser = async (username, password) => {
   try {
     const result = await usernameModel.create({
       username,
-      password
-    })
-    return result
+      password,
+    });
+    return result;
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 const login = async (username, password) => {
   try {
     const findUser = await usernameModel.findOne({
       where: {
-        username
-      }
-    })
+        username,
+      },
+    });
 
     if (!findUser) throw new Error('invalid username / password');
 
-    const { 
+    const {
       username: dbUser,
       password: dbPass,
-    } = findUser
+    } = findUser;
 
-    const checkPassword = comparePass(password, dbPass)
+    const checkPassword = comparePass(password, dbPass);
 
     if (!checkPassword) throw new Error('invalid username / password');
 
     const forToken = {
       dbUser,
-      dbPass
-    }
+      dbPass,
+    };
 
-    const token = jwtSign(forToken)
+    const token = jwtSign(forToken);
 
-    return token
+    return token;
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 const getToken = async (paramkey) => {
   try {
-    if (paramkey == shortcut) paramkey = key
+    // eslint-disable-next-line no-param-reassign
+    if (paramkey === shortcut) paramkey = key;
     const forToken = {
-      paramkey
-    }
-    const token = jwtSign(forToken)
-    return token
+      paramkey,
+    };
+    const token = jwtSign(forToken);
+    return token;
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
 
 module.exports = {
   createUser,
   login,
-  getToken
-}
+  getToken,
+};
--- routes/v1/user.js
+++ routes/v1/user.js
@@ -7,21 +7,21 @@ const userController = {
       req.checkBody({
         username: { notEmpty: true, errorMessage: 'username is required' },
         password: { notEmpty: true, errorMessage: 'password is required' },
-      })
+      });
 
       const errors = req.validationErrors();
       if (errors) {
         return res.status(400).json({
-          err: errors
-        })
+          err: errors,
+        });
       }
-      const {username, password} = req.body
-      const result = await createUser(username, password)
+      const { username, password } = req.body;
+      const result = await createUser(username, password);
       return res.status(201).json({
-        result
-      })
+        result,
+      });
     } catch (error) {
-      return res.status(500).json(error)
+      return res.status(500).json(error);
     }
   },
   login: async (req, res) => {
@@ -29,49 +29,49 @@ const userController = {
       req.checkBody({
         username: { notEmpty: true, errorMessage: 'username is required' },
         password: { notEmpty: true, errorMessage: 'password is required' },
-      })
+      });
 
       const errors = req.validationErrors();
       if (errors) {
         return res.status(400).json({
-          err: errors
-        })
+          err: errors,
+        });
       }
-      const {username, password} = req.body
+      const { username, password } = req.body;
 
-      const token = await login(username, password)
+      const token = await login(username, password);
       return res.status(200).json({
-        token
-      })
+        token,
+      });
     } catch (error) {
-      return res.status(500).json({message: error.message})
+      return res.status(500).json({ message: error.message });
     }
   },
   token: async (req, res) => {
     try {
       req.checkBody({
         key: { notEmpty: true, errorMessage: 'key is required' },
-      })
+      });
 
       const errors = req.validationErrors();
       if (errors) {
         return res.status(400).json({
-          err: errors
-        })
+          err: errors,
+        });
       }
-      const {key} = req.body
-      const token = await getToken(key)
+      const { key } = req.body;
+      const token = await getToken(key);
       return res.status(200).json({
-        token
-      })
+        token,
+      });
     } catch (error) {
-      return res.status(500).json({message: error.message})
+      return res.status(500).json({ message: error.message });
     }
-  }
-}
+  },
+};
 
 module.exports = (router) => {
   router.post('/register', auth, userController.signUp);
   router.post('/login', userController.login);
   router.post('/getToken', userController.token);
-}
+};
